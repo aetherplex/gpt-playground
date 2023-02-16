0679797502ee486fa29e85e51be4259008bc6230
daoarchitect <dom@optiodao.xyz>
2023-02-16 17:28:41 +1000

    fix: prisma issue

--- server/prisma.ts
+++ server/prisma.ts
@@ -1,22 +1,18 @@
-/**
- * Instantiates a single instance PrismaClient and save it on the global object.
- * @link https://www.prisma.io/docs/support/help-articles/nextjs-prisma-client-dev-practices
- */
-
 import { PrismaClient } from '@prisma/client';
-const prismaGlobal = global as typeof global & {
-    prisma?: PrismaClient;
-};
 
-export const prisma: PrismaClient =
-    prismaGlobal.prisma ||
-    new PrismaClient({
-        log:
-            process.env.NODE_ENV === 'development'
-                ? ['query', 'error', 'warn']
-                : ['error'],
-    });
-
-if (process.env.NODE_ENV !== 'production') {
-    prismaGlobal.prisma = prisma;
+declare global {
+    var prisma: PrismaClient;
 }
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === 'production') {
+    prisma = new PrismaClient();
+} else {
+    if (!global.prisma) {
+        global.prisma = new PrismaClient();
+    }
+    prisma = global.prisma;
+}
+
+export default prisma;
--- server/routers/_app.ts
+++ server/routers/_app.ts
@@ -1,5 +1,5 @@
 import { z } from 'zod';
-import { prisma } from '../prisma';
+import prisma from '../prisma';
 import { procedure, router } from '../trpc';
 
 export const AppRouter = router({
