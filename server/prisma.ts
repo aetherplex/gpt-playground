5201f8c35d0249c10f027b72bce8ac45ed266d4e
daoarchitect <dom@optiodao.xyz>
2023-02-16 17:31:37 +1000

    fix: prisma issue

--- server/prisma.ts
+++ server/prisma.ts
@@ -1,18 +1,3 @@
 import { PrismaClient } from '@prisma/client';
-
-declare global {
-    var prisma: PrismaClient;
-}
-
-let prisma: PrismaClient;
-
-if (process.env.NODE_ENV === 'production') {
-    prisma = new PrismaClient();
-} else {
-    if (!global.prisma) {
-        global.prisma = new PrismaClient();
-    }
-    prisma = global.prisma;
-}
-
+const prisma: PrismaClient = new PrismaClient();
 export default prisma;
